a021b56106f6e42778ff32fd2ff6a262bbc046fb
Shruti yadav <81573749+Shruti042003@users.noreply.github.com>
2021-06-09 09:47:27 +0530

    Print Fibonacci Numbers Till N

---
+++ Fibonacciseries.java
@@ -0,0 +1,18 @@
+/*1. You've to print first n fibonacci numbers.
+2. Take as input "n", the count of fibonacci numbers to print.
+3. Print first n fibonacci numbers.*/
+import java.util.Scanner;
+public class Fibonacciseries {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int a = 0;
+        int b = 1;
+        for(int i = 1; i <= n; i++){
+            System.out.println(a);
+            int c = a + b;
+            a = b;
+            b = c;
+        }   
+    }
+}
