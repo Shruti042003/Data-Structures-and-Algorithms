33e195fe49f3cdd51c8d0d0e67ef1205159d20cb
Shruti yadav <81573749+Shruti042003@users.noreply.github.com>
2021-06-10 09:48:13 +0530

    Diamond triangle pattern

---
+++ Pattern5.java
@@ -0,0 +1,45 @@
+ 
+ /*
+        *	
+    *	*	*	
+*	*	*	*	*	
+    *	*	*	
+        *	
+*/
+import java.util.Scanner;
+public class Pattern5{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();        
+        int mid = (n / 2) + 1;
+        int mid_term = mid - 1;
+        int nstar = 1;
+        for(int i = 1; i <= mid; i++){
+            for(int j = 1; j <= mid_term; j++){
+                System.out.print("\t");
+            }
+            mid_term -= 1;
+            for(int k = 1; k <= nstar; k++){
+                System.out.print("*\t");        
+            }
+            nstar += 2;
+            System.out.println( );        
+        }
+        mid_term = 1;
+        nstar = n;
+        
+        for(int i = 1; i < mid; i++){
+            for(int j = 1; j <= mid_term; j++){
+                System.out.print("\t");
+            }
+            mid_term += 1;
+            nstar -= 2;
+            for(int k = 1; k <= nstar; k++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
